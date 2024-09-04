790fadd0d1d9d63c8873cd80130f72d4c6544c4e
VerCion <47823914+VerCion@users.noreply.github.com>
2024-09-05 07:22:56 +0800

    github page fix v1.0

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/christiananthony19/MeetAndFlick/', // Add this line
+  base: '/MeetAndFlick/', // Add this line
 })
